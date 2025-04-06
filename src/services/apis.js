2edc7ea9121ab85bb9efab82d8b005201c22512e
Avinash Kumar <avinash.k95347@gmail.com>
2025-04-07 01:08:48 +0530

    fix

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL='http://localhost:4000/api/v1'
+const BASE_URL = window.location.origin.includes('localhost') ? 'http://localhost:4000/api/v1' : 'https://courseyard.onrender.com/api/v1'
 
 // AUTH ENDPOINTS
 export const endpoints = {
