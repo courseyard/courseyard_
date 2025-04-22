4b71e0b3ebfdf1e5773a81663edafdbc747efd85
Avinash Kumar <avinash.k95347@gmail.com>
2025-04-22 18:19:29 +0530

    fix

--- server/mail/templates/paymentSuccessEmail.js
+++ server/mail/templates/paymentSuccessEmail.js
@@ -67,7 +67,7 @@ exports.paymentSuccessEmail = (name, amount, orderId, paymentId) => {
     
     <body>
         <div class="container">
-            <a href="https://Courseyard-edtech-project.vercel.app"><img class="logo" src="https://i.ibb.co/7Xyj3PC/logo.png"
+            <a href="https://courseyard-ten.vercel.app"><img class="logo" src="https://i.ibb.co/7Xyj3PC/logo.png"
                     alt="Courseyard Logo"></a>
             <div class="message">Course Payment Confirmation</div>
             <div class="body">
